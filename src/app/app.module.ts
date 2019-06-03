417834f1f12110b47492a6b00b6ee4da9d22ce28
Samanta Lara <slaragiannone@gmail.com>
2019-06-03 15:37:44 +0100

    card component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { AddTaskComponent } from './add-task/add-task.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AddTaskComponent } from './add-task/add-task.component';
     NavbarComponent,
     HomeComponent,
     TasksComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
